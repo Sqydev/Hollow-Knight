4ac17a2780c05b72727545193757e09ad83ce0b4
Sqyd <wojkap10@proton.me>
2025-09-10 17:44:15 +0200

    Made main menu(I couldn't find buttons assets so they are raw)

--- scripts/Menu/main.gd
+++ scripts/Menu/main.gd
@@ -13,11 +13,16 @@ func _process(delta: float) -> void:
 
 	if(playbt.button_pressed):
 		print_debug("Play button pressed\n")
+		get_tree().change_scene_to_file("res://scenes/Menu/save_select.tscn")
 	if(settingsbt.button_pressed):
 		print_debug("Settings button pressed\n")
+		get_tree().change_scene_to_file("res://scenes/Menu/settings.tscn")
 	if(advancmentsbt.button_pressed):
 		print_debug("Advancments button pressed\n")
+		get_tree().change_scene_to_file("res://scenes/Menu/advancments.tscn")
 	if(extrasbt.button_pressed):
 		print_debug("Extras button pressed\n")
+		get_tree().change_scene_to_file("res://scenes/Menu/extras.tscn")
 	if(quitbt.button_pressed):
 		print_debug("Quit button pressed\n")
+		get_tree().quit()

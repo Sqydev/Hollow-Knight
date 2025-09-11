c49345285dd61b6faf101e926d7a849bad803192
Sqyd <wojkap10@proton.me>
2025-09-11 14:47:08 +0200

    Pull request next 2

--- scripts/Scene_Triggers/SceneChaingeTrigger.gd
+++ scripts/Scene_Triggers/SceneChaingeTrigger.gd
@@ -4,7 +4,7 @@ extends Node2D
 @onready var Fame = $CanvasLayer/ColorRect
 
 func _ready() -> void:
-	await Fame.fade_in(0.5)
+	await Fame.fade_in(0.2)
 	Trigger.body_entered.connect(FilterToPlayer)
 
 func _process(delta: float) -> void:
@@ -19,6 +19,6 @@ func FilterToPlayer(body):
 	if(body.name == "The Knight"):
 		print_debug("Scene_Change to: ", NewScene)
 		
-		await Fame.fade_out(0.5)
+		await Fame.fade_out(0.2)
 		
 		get_tree().change_scene_to_file(NewScene)

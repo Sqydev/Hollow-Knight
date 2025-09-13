49a4a7f73a6004b5c194985fb31db9c2468e122e
Sqyd <wojkap10@proton.me>
2025-09-14 00:12:44 +0200

    Made simple NewFile system

--- scripts/Save System/Save_System.gd
+++ scripts/Save System/Save_System.gd
@@ -1,24 +1,41 @@
 extends Node
 
-func readySave(Mode):
+var SaveFolder
+var FileSlotGlob
+
+func readySave(Mode, FileSlot):
 	var KelnerOfUser = OS.get_name();
+	FileSlotGlob = FileSlot
 	
 	if(KelnerOfUser == "Windows"):
-		const SaveFolder = "user://HollowKnightRecreation/";
+		SaveFolder = "user://HollowKnightRecreation/";
 	elif(KelnerOfUser == "Linux"):
-		const SaveFolder = "~/.local/HollowKnightRecreation/";
+		SaveFolder = "~/.local/HollowKnightRecreation/";
 	else:
 		print("Os unsuported");
 	
 	if(Mode == 1):
 		NewFile();
+		OpenSave();
 	elif(Mode == 2):
-		SaveFile();
+		OpenSave();
 	elif(Mode == 3):
+		SaveFile();
+	elif(Mode == 4):
 		DeleteFile();
 
 func NewFile():
-	pass;
+	if(DirAccess.open(SaveFolder) == null):
+		DirAccess.make_dir_absolute(SaveFolder);
+		NewFile();
+	else:
+		var file = FileAccess.open(SaveFolder + FileSlotGlob + ".SAVETHEWORLD", FileAccess.WRITE);
+		if(file != null):
+			file.store_string("NEWFILE;")
+			file.close()
+
+func OpenSave():
+	pass
 
 func SaveFile():
 	pass;

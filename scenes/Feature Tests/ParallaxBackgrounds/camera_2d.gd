e71a7f92ded3ddbce5cf5e73b2f003b97cc1ba8f
Mint <mici5dabestpl@gmail.com>
2025-09-16 12:23:02 +0200

    Added a parallax background testing scene, alongside a placeholder for it.

---
+++ scenes/Feature Tests/ParallaxBackgrounds/camera_2d.gd
@@ -0,0 +1,9 @@
+extends Camera2D
+
+func _physics_process(delta: float) -> void:
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+	self.position.x += 10*direction
